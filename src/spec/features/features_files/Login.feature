413831e0f2e6494b433e41f3aa143b127ec8e9ca
samiakhalfi1718 <samia.khalfi.1718@gmail.com>
2023-05-13 00:31:17 +0100

    update project

---
+++ src/spec/features/features_files/Login.feature
@@ -0,0 +1,11 @@
+@login
+Feature: Je souhaite tester la page de la connexion de l'application Swag labs
+  ETQ utilisateur je souhaite tester la pase de la connexion de l'application Swag Labs
+
+  @connexion-CasPassant
+  Scenario: Je souhaite tester la page de la connexion avec un cas passant
+    Given Je me connecte a l application Swag Labs
+    When Je saisi le username "standard_user"
+    And Je saisi le password "secret_sauce"
+    And Je clique sur le bouton Login
+    Then Je me redirige vers la page Home "Products"
--- src/test/java/com/e2etest/automation/RunWebSuiteTest.java
+++ src/test/java/com/e2etest/automation/RunWebSuiteTest.java
@@ -1,5 +1,39 @@
 package com.e2etest.automation;
 
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import static io.cucumber.junit.CucumberOptions.SnippetType.CAMELCASE;
+
+/**
+ * 
+ * The Test Runner File uses the @RunWith() Annotation from JUnit for executing
+ * tests.
+ * 
+ */
+@RunWith(Cucumber.class)
+/**
+ * 
+ * The @CucumberOptions Annotation is used to define the location of feature
+ * files, step definitions, reporting
+ * 
+ * integration.
+ * 
+ */
+@CucumberOptions(
+		features = { "src/spec/features" }, 
+		plugin = { "pretty","html:target/cucumber-report.html" }, 
+		tags = ("@login"),
+		// glue= {"src/test/java/com/e2etest/automation/step_definitions"},
+		monochrome = true, 
+		snippets = CAMELCASE)
+
+/**
+ * This class is used to run the test, which is a JUnit Test Runner Class
+ * containing the Step Definition location and the other primary metadata
+ * required to run the test.
+ */
+
 public class RunWebSuiteTest {
 
 }
